e7884e5182e0f50f0a7ffd7f9e31ca69427a4e6b
Islam Rustamov <rustamov.exceedteam@gmail.com>
2020-04-09 14:31:39 +0300

    Remove config.js from gitignore

---
+++ graphql-app/config.js
@@ -0,0 +1,9 @@
+const MONGODB =
+  'mongodb+srv://islamrustamov:621998islam@islamrustamov-brvnk.mongodb.net/test?retryWrites=true&w=majority';
+
+const SECRET = 'somesalt';
+
+const NODEMAILER_EMAIL = 'swigge@mail.ru';
+const NODEMAILER_PASSWORD = '666420renard';
+
+module.exports = { MONGODB, SECRET, NODEMAILER_PASSWORD, NODEMAILER_EMAIL };
